274e6fda8a48fdf3ea6fb2915847e1a6a925a751
Adryan <adryan.campos@escola.pr.gov.br>
2026-02-23 13:45:08 +0000

    aula 02

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, SafeAreaView } from 'react-native';
-import Sobrenome from './src /Telas/Sobrenome';
-
+import { Text, SafeAreaView,} from 'react-native';
+import Cesta from './src /Telas/Cesta';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <Text>Ap de 3,50</Text>
-      <Sobrenome nome='full ump e xm8'/>
-      <Sobrenome nome='Vale duas ump adm?'/>
-      <Sobrenome nome='W.O adm'/>
-      <Sobrenome nome='Pode Música adm?'/>
-      <Sobrenome nome='Pode se ensconder adm?'/>
-      <Sobrenome nome='Pode falar da mãe adm?'/>
-      
-     <StatusBar />
-    </SafeAreaView>
-  );
+return (
+<SafeAreaView >
+<Cesta />
+
+</SafeAreaView>
+);
 }
-
-
